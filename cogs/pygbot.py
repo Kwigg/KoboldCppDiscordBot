8c3262cbfcd87f7a4585d686fbc96103673928bf
kwigg <Kwigg@users.noreply.github.com>
2023-07-01 09:41:48 +0100

    Replace conversation.history with token-counted list.

--- cogs/pygbot.py
+++ cogs/pygbot.py
@@ -5,6 +5,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import os
+import sentencepiece
 
 # configuration settings for the api
 model_config = {
@@ -15,7 +16,7 @@ model_config = {
     "use_memory": False,
     # LLaMA tuned params
     "max_context_length": 2048,
-    "max_length": 300,
+    "max_length": 150,
     "rep_pen": 1.19,
     "rep_pen_range": 1024,
     "rep_pen_slope": 0.9,
@@ -32,11 +33,14 @@ model_config = {
 
 seen_users = []
 
-
 def embedder(msg):
     embed = discord.Embed(description=f"{msg}", color=0x9C84EF)
     return embed
 
+def count_tokens_in_string(eval_string):
+    sp = sentencepiece.SentencePieceProcessor(model_file='tokenizer_model/tokenizer.model')
+    tokens = sp.encode_as_ids(eval_string)
+    return len(tokens)
 
 class Chatbot:
     def __init__(self, char_filename, bot):
@@ -66,14 +70,42 @@ class Chatbot:
 
         # initialize conversation history and character information
         self.convo_filename = None
-        self.conversation_history = ""
         self.character_info = f"{self.char_name}'s Persona: {self.char_persona}\n"
         if self.personality is not None:
             self.character_info += (f"Description of {self.char_name}: {self.personality}\n")
         if self.world_scenario is not None:
-            self.character_info += f"Scenario: {self.world_scenario}\n"
+            self.character_info += f"Scenario: {self.world_scenario}\n<START>"
 
-        self.num_lines_to_keep = 30
+        self.char_info_tokens = count_tokens_in_string(self.character_info)
+        self.conversation_queue = []
+        self.conversation_queue_total_token = 0
+
+    def dequeue_item_and_update_token_count(self):
+        line = self.conversation_queue.pop()
+        self.conversation_queue_total_token -= line[1]
+
+    def remove_item_at_index_and_update_token_count(self, index):
+        self.conversation_queue_total_token -= self.conversation_queue[index][1]
+        del self.conversation_queue[index]
+
+    def extract_prompt_out_of_queue(self):
+        prompt_string = ""
+        for line in self.conversation_queue:
+            prompt_string = line[0] + "\n" + prompt_string # The queue is in reverse, with the last element being the oldest
+        return prompt_string
+
+    def push_lines_to_queue_respecting_context_limits(self, in_strings):
+        print(in_strings)
+        for input_line in in_strings: 
+            string_token_count = count_tokens_in_string(input_line)
+            # Need to clear items until our new string fits
+            while (self.conversation_queue_total_token + self.char_info_tokens + string_token_count) > (model_config["max_context_length"] - model_config["max_length"]):
+                self.dequeue_item_and_update_token_count()
+
+            self.conversation_queue.insert(0, (input_line, string_token_count))
+            self.conversation_queue_total_token += string_token_count
+    def push_single_line_to_queue_respecting_context_limits(self, in_string):
+        self.push_lines_to_queue_respecting_context_limits([in_string])
 
     async def set_convo_filename(self, convo_filename):
         # set the conversation filename and load conversation history from file
@@ -83,9 +115,8 @@ class Chatbot:
             return
         with open(convo_filename, "r", encoding="utf-8") as f:
             lines = f.readlines()
-            num_lines = min(len(lines), self.num_lines_to_keep)
-            self.conversation_history = "<START>\n" + "".join(lines[-num_lines:])
-
+            self.push_lines_to_queue_respecting_context_limits(lines)
+        
     async def reset_convo_file(self):
         print(self.convo_filename)
         # set the conversation filename and load conversation history from file
@@ -94,12 +125,14 @@ class Chatbot:
         start_dialogue = ""
         if self.example_dialogue is not None:
             start_dialogue = "Example Dialogue: " + self.example_dialogue + "\n"
+        start_dialogue += "<START>\n"
         with open(self.convo_filename, "w", encoding="utf-8") as f:
-            f.write(start_dialogue + "<START>\n")
-        self.conversation_history = start_dialogue + "<START>\n"
+            f.write(start_dialogue)
+        self.push_lines_to_queue_respecting_context_limits(start_dialogue.splitlines())
         return True
 
     async def send_prompt_and_parse_result(self, message):
+        print(self.conversation_queue)
         print(message)
         if message is not None:
             if message.author.nick not in seen_users:
@@ -108,8 +141,8 @@ class Chatbot:
                     model_config["stop_sequence"].append(message.author.nick + ":")
                 print(seen_users)
         message_prompt = {
-            "prompt": self.character_info +
-            "\n".join(self.conversation_history.split("\n")[-self.num_lines_to_keep :]) +
+            "prompt": self.character_info + "\n" + 
+            "".join(self.extract_prompt_out_of_queue()) +
             f"{self.char_name}:",
         }
         combined_prompt = {**message_prompt, **model_config}
@@ -136,13 +169,13 @@ class Chatbot:
         return (response, response_text)
 
     async def save_conversation(self, message, message_content):
-        self.conversation_history += f"{message.author.nick}: {message_content}\n"
+        self.push_single_line_to_queue_respecting_context_limits(f"{message.author.nick}: {message_content}")
         # send a post request to the API endpoint
         (response, response_text) = await self.send_prompt_and_parse_result(message)
         # check if the request was successful
         if response.status_code == 200:
             # add bot response to conversation history
-            self.conversation_history = (self.conversation_history + f"{self.char_name}: {response_text}\n")
+            self.push_single_line_to_queue_respecting_context_limits(f"{self.char_name}: {response_text}")
             with open(self.convo_filename, "a", encoding="utf-8") as f:
                 f.write(f"{message.author.nick}: {message_content}\n")
                 f.write(f"{self.char_name}: {response_text}\n")  # add a separator between
@@ -150,11 +183,10 @@ class Chatbot:
             return response_text
 
     async def follow_up(self):
-        self.conversation_history = self.conversation_history
         (response, response_text) = await self.send_prompt_and_parse_result(None)
         # check if the request was successful
         if response.status_code == 200:
-            self.conversation_history = (self.conversation_history + f"{self.char_name}: {response_text}\n")
+            self.push_single_line_to_queue_respecting_context_limits(f"{self.char_name}: {response_text}")
             with open(self.convo_filename, "a", encoding="utf-8") as f:
                 f.write(
                     f"{self.char_name}: {response_text}\n"
@@ -218,47 +250,48 @@ class ChatbotCog(commands.Cog, name="chatbot"):
         await interaction.delete_original_response()
         await interaction.channel.send(await self.chatbot.follow_up())
 
-    @app_commands.command(name="regenerate", description="regenerate last message")
-    async def regenerate(self, interaction: discord.Interaction) -> None:
-        await interaction.response.defer()
-        await interaction.delete_original_response()
-        if interaction.guild:
-            server_name = interaction.channel.name
-        else:
-            server_name = interaction.author.name
-        chatlog_filename = os.path.join( self.chatlog_dir, f"{self.chatbot.char_name}_{server_name}_chatlog.log")
-        if ((interaction.guild and self.chatbot.convo_filename != chatlog_filename) or
-            (not interaction.guild and self.chatbot.convo_filename != chatlog_filename)
-        ):
-            await self.chatbot.set_convo_filename(chatlog_filename)
-        # Get the last message sent by the bot in the channel
-        async for message in interaction.channel.history(limit=1):
-            if message.author == self.bot.user:
-                await message.delete()
-                lines = self.chatbot.conversation_history.splitlines()
+    """
+
+    I can't be arsed to deal with this function right now
+
+        @app_commands.command(name="regenerate", description="regenerate last message")
+        async def regenerate(self, interaction: discord.Interaction) -> None:
+            await interaction.response.defer()
+            await interaction.delete_original_response()
+            if interaction.guild:
+                server_name = interaction.channel.name
+            else:
+                server_name = interaction.author.name
+            chatlog_filename = os.path.join( self.chatlog_dir, f"{self.chatbot.char_name}_{server_name}_chatlog.log")
+            if ((interaction.guild and self.chatbot.convo_filename != chatlog_filename) or
+                (not interaction.guild and self.chatbot.convo_filename != chatlog_filename)
+            ):
+                await self.chatbot.set_convo_filename(chatlog_filename)
+            # Get the last message sent by the bot in the channel
+            async for message in interaction.channel.history(limit=1):
+                if message.author == self.bot.user:
+                    await message.delete()
+                    for i in range(len(lines) - 1, -1, -1):
+                        if lines[i].startswith(f"{self.chatbot.char_name}:"):
+                            self.chatbot.remove_item_at_index_and_update_token_count(i)
+                            break
+                    break  # Exit the loop after deleting the message
+            with open(self.chatbot.convo_filename, "r", encoding="utf-8") as f:
+                lines = f.readlines()
+                # Find the last line that matches "self.chatbot.char_name: {message.content}"
+                last_line_num_to_overwrite = None
                 for i in range(len(lines) - 1, -1, -1):
-                    if lines[i].startswith(f"{self.chatbot.char_name}:"):
-                        lines[i] = f"{self.chatbot.char_name}:"
-                        self.chatbot.conversation_history = "\n".join(lines)
-                        self.chatbot.conversation_history = (self.chatbot.conversation_history)
+                    if f"{self.chatbot.char_name}: {message.content}" in lines[i]:
+                        last_line_num_to_overwrite = i
                         break
-                print(f"string after: {repr(self.chatbot.conversation_history)}")
-                break  # Exit the loop after deleting the message
-        with open(self.chatbot.convo_filename, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            # Find the last line that matches "self.chatbot.char_name: {message.content}"
-            last_line_num_to_overwrite = None
-            for i in range(len(lines) - 1, -1, -1):
-                if f"{self.chatbot.char_name}: {message.content}" in lines[i]:
-                    last_line_num_to_overwrite = i
-                    break
-            if last_line_num_to_overwrite is not None:
-                lines[last_line_num_to_overwrite] = ""
-                # Modify the last line that matches "self.chatbot.char_name: {message.content}"
-            with open(self.chatbot.convo_filename, "w", encoding="utf-8") as f:
-                f.writelines(lines)
-                f.close()
-        await interaction.channel.send(await self.chatbot.follow_up())
+                if last_line_num_to_overwrite is not None:
+                    lines[last_line_num_to_overwrite] = ""
+                    # Modify the last line that matches "self.chatbot.char_name: {message.content}"
+                with open(self.chatbot.convo_filename, "w", encoding="utf-8") as f:
+                    f.writelines(lines)
+                    f.close()
+            await interaction.channel.send(await self.chatbot.follow_up())
+    """
 
     async def api_get(self, parameter):
         response = requests.get(f"{self.chatbot.endpoint}/api/v1/config/{parameter}")
